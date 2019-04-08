f86c9a4bdc9339aecd151da997871f2209a096b9
calebfergie <yonohabloespanol@gmail.com>
2019-04-08 16:51:35 -0400

    setting minor edit to 0

--- LocalSettings.php
+++ LocalSettings.php
@@ -165,7 +165,7 @@ wfLoadExtension( 'WikiEditor' );
 wfLoadExtension( 'LinkTitles' );
 # Create links when pages are created
 # Needs to have minor checkbox edit undefaulted - but breaks button links, so disabling for now
-$wgDefaultUserOptions['minordefault'] = 1;
+$wgDefaultUserOptions['minordefault'] = 0;
 $wgLinkTitlesParseOnEdit = true;
 # remove case sensitivity with smart mode
 $wgLinkTitlesSmartMode = true;

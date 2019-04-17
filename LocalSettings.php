ffd6cbdc57d4a8b877e2e9521d71d447f994c951
calebfergie <yonohabloespanol@gmail.com>
2019-04-17 15:41:19 -0400

    removing old extensions and adding GTM headscript

--- LocalSettings.php
+++ LocalSettings.php
@@ -144,10 +144,11 @@ $wgGroupPermissions['sysop']['masseditregex'] = true;
 
 # Google Analytics Tracking with Headscript
 require_once "$IP/extensions/HeadScript/HeadScript.php";
-## GA code in headscript
+## GA & GTM code in headscript
 $wgHeadScriptCode = <<<'START_END_MARKER'
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138072554-1"></script>
 <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-138072554-1');</script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5GZQV9N');</script>
 START_END_MARKER;
 
 #AWS Extension - for image storage
--- extensions/Contributors/includes/sql/contributors-add-timestamps.sql
+++
@@ -1,5 +0,0 @@
--- Add two columns for the first edit and last edit of a contributor
-ALTER TABLE /*_*/contributors
-  ADD COLUMN cn_first_edit varbinary(14) ;
-ALTER TABLE /*_*/contributors
-  ADD COLUMN cn_last_edit varbinary(14) ;
